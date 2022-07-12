94ab9ae5b57baa617809f4a4fe7c92165c25d891
m4nh <degregorio.daniele@gmail.com>
2022-07-12 18:27:08 +0200

    Working fox optimizer with SIFT

--- scripts/optimize_poses/optimize_pose_kps.py
+++ scripts/optimize_poses/optimize_pose_kps.py
@@ -24,6 +24,7 @@ from raft import RAFT
 from utils import flow_viz
 from utils.utils import InputPadder
 from types import SimpleNamespace
+import imageio
 
 DEVICE = "cuda"
 
@@ -91,13 +92,119 @@ class Transforms3DUtils(object):
         return T
 
 
+class ImagePairsLoss:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, img1: np.ndarray, img2: np.ndarray) -> float:
+        raise NotImplementedError("ImagePairsLoss is not implemented")
+
+
+class FeaturesImageLoss:
+    def __init__(
+        self,
+        algo: str = "sift",
+        expected_max_matches: int = 100,
+        max_matches_weights: float = 10,
+        debug: bool = True,
+    ) -> None:
+
+        self._algo = algo
+        self._expected_max_matches = expected_max_matches
+        self._max_matches_weights = max_matches_weights
+        self._debug = debug
+
+    def __call__(self, img1: np.ndarray, img2: np.ndarray) -> float:
+
+        img1 = (img1 * 255).astype(np.uint8)
+        img2 = (img2 * 255).astype(np.uint8)
+
+        if self._algo == "sift":
+
+            sift = cv2.SIFT_create()
+
+            # detect / describe
+            kp1, des1 = sift.detectAndCompute(img1, None)
+            kp2, des2 = sift.detectAndCompute(img2, None)
+
+            # Compute matches
+            bf = cv2.BFMatcher()
+            matches = bf.knnMatch(des1, des2, k=2)
+
+            # Apply ratio test
+            good = []
+            max_distance = 0.0
+            for m, n in matches:
+                if m.distance > max_distance:
+                    max_distance = m.distance
+                if n.distance > max_distance:
+                    max_distance = n.distance
+                if m.distance < 0.6 * n.distance:
+                    good.append([m])
+
+            # Compute average keypoints distance loss
+            loss = 0.0
+            for g in good:
+                p1 = np.array(kp1[g[0].queryIdx].pt)
+                p2 = np.array(kp2[g[0].trainIdx].pt)
+                norm_distance = max_distance / g[0].distance
+
+                loss += (np.linalg.norm(p1 - p2) * (1 / norm_distance)) ** 2
+
+            loss /= len(good)  # normalize distance loss on number of matches
+
+            # increase loss proportional to the inverse of number of matches
+            # More matches -> Lower loss
+            loss += self._max_matches_weights * max(
+                (self._expected_max_matches - len(good)) / self._expected_max_matches, 0
+            )
+
+            if self._debug:
+                img3 = cv2.drawMatchesKnn(
+                    img1,
+                    kp1,
+                    img2,
+                    kp2,
+                    good,
+                    None,
+                    flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+                )
+                cv2.imshow("matches", img3)
+                cv2.waitKey(1)
+
+            return loss
+        else:
+            raise NotImplementedError("Algo {} is not implemented".format(self._algo))
+
+
+class FrameWriter:
+    def __init__(self, output_folder: str):
+        self.active = False
+        self.counter = 0
+        if len(output_folder) > 0:
+            self.output_folder = Path(output_folder)
+            self.output_folder.mkdir(parents=True, exist_ok=True)
+            self.active = True
+
+    def __call__(self, frame: np.ndarray):
+        if self.active:
+            filename = self.output_folder / f"{self.counter:05d}.png"
+            rich.print(
+                "Frame saved to", filename, frame.min(), frame.max(), frame.dtype
+            )
+            cv2.imwrite(str(filename), frame)
+            self.counter += 1
+
+
 def optimize_pose(
     transforms_file: Path = t.Option(..., help="transform json file "),
-    start_pose_index: int = t.Option(0, help="start pose index"),
-    end_pose_index: int = t.Option(10, help="end pose index"),
+    start_pose_index: int = t.Option(12, help="start pose index"),
+    end_pose_index: int = t.Option(22, help="end pose index"),
     neural_twin_file: Path = t.Option(..., help="Neural Twin File"),
     spp: int = t.Option(1, help="Number of Samples per Pixel used for rendering"),
     ngp_build_folder: Path = t.Option("build", help="Folder in which Instant is built"),
+    fov: float = t.Option(40.0, help="Field of View"),
+    output_folder: str = t.Option("", help="Output folder"),
 ):
     import sys
 
@@ -107,6 +214,8 @@ def optimize_pose(
     import numpy as np
     import json
     import cv2
+
+    wr = FrameWriter(output_folder)
 
     # Selecting mode
     mode = ngp.TestbedMode.Nerf
@@ -149,7 +258,7 @@ def optimize_pose(
     width, height = 256, 256
     print("SIZE", width, height)
     testbed.fov_axis = 0
-    testbed.fov = 40
+    testbed.fov = fov
 
     def render_frame(T: np.ndarray, height: int, width: int):
         testbed.set_nerf_camera_matrix(T[:-1, :])
@@ -161,6 +270,10 @@ def optimize_pose(
         )[:, :, :3]
 
     start_image = render_frame(T_start, height, width)
+
+    # start_image = (
+    #     imageio.imread("/home/daniele/Downloads/00035_marker.png")[:, :, :3] / 255.0
+    # ).astype(np.float32)
     target_image = render_frame(T_end, height, width)
 
     OUTPUT = lambda x: cv2.cvtColor(x, cv2.COLOR_RGB2BGR)
@@ -217,7 +330,8 @@ def optimize_pose(
         )
 
         try:
-            sift = cv2.SIFT_create()
+            # sift = cv2.ORB_create(nfeatures=2000)
+            sift = cv2.SIFT_create(nfeatures=2000)
             # find the keypoints and descriptors with SIFT
             img1 = (start_image * 255).astype(np.uint8)
             img2 = (end_image * 255).astype(np.uint8)
@@ -261,15 +375,18 @@ def optimize_pose(
                 None,
                 flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
             )
+
             cv2.imshow("matches", img3)
             cv2.imshow("stack", stack)
+            wr((stack * 255).astype(np.uint8))
+
             k = cv2.waitKey(1)
             if ord("q") == k:
                 sys.exit(0)
 
             fitness = match_distances
 
-            if optim_counter > 1500:
+            if optim_counter > 800:
                 fitness += 1000 * np.abs(start_image - end_image).mean()
 
             print("F", fitness, f"({optim_counter})")
@@ -285,6 +402,7 @@ def optimize_pose(
     translational_bound = 3
 
     # Choose optimizer -> https://facebookresearch.github.io/nevergrad/optimization.html#choosing-an-optimizer
+    # optimizer = ng.optimizers.CMA(
     optimizer = ng.optimizers.CMA(
         parametrization=ng.p.Array(init=[0, 0, 0, 0, 0, 0]).set_bounds(
             [
